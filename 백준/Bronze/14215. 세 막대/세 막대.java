128bce973bcb86d4a041edca4ccf06db2255fb87
rkdmf1102 <151818165+rkdmf1102@users.noreply.github.com>
2024-04-01 12:00:43 +0900

    [Bronze III] Title: 세 막대, Time: 204 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] a = new int[3];
+		
+		for(int i = 0; i<3; i++) {
+			a[i]=sc.nextInt();
+		}
+		
+		Arrays.sort(a);
+		
+		if(a[0]+a[1]<=a[2]) {
+			a[2]=a[0]+a[1]-1;
+		}
+		
+		System.out.println(a[0]+a[1]+a[2]);
+	}
+}
